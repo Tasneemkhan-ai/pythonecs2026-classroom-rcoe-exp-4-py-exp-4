1351fe287c17582c8e71760c058f3c23bbfda252
Tasneem <khantasneem647@gmail.com>
2026-03-02 22:26:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 # AIM: Write a Python program to calculate the simple interest based on user input.
-# Coder:
-# Date:
+# Coder:Tasneem Khan
+# Date:02-03-2026
 
 # Write your code here
+print("---Simple Interest Calculator---")
+#To take principal from the user
+principal=float(input("Enter the principal:"))
+#To take interest from the user
+intrst=float(input("Enter the rate of interest:"))
+#TO take time period from the user
+time=float(input("Enter the time period(in years):"))
+#formula of simple interest
+SI=(principal*intrst*time)/100
+print("Simple interst =",SI)                                        
