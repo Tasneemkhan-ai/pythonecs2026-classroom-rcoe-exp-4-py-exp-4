7f8355686a95895b24c12e881ff10004237c2a60
Tasneem <khantasneem647@gmail.com>
2026-04-14 22:33:56 +0530

    Implement simple interest calculation with user input
    Added user input for principal amount, rate, and time to calculate simple interest.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # AIM: Write a Python program to calculate the simple interest based on user input.
-# Coder:
-# Date:
+# Coder: Tasneem Khan 
+# Date: 16/2/26
 
 # Write your code here
+amt=float(input("Enter Principal Amount: "))
+rate=float(input("Enter Rate of Interest: "))
+time=float(input("Enter Time Period in Years: "))
+si=amt*rate*time/100	#calculating simple interest
+
+print(f"Simple Interest: {si}")
